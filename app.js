47a77e028630014c0f9b96e6869d6fca5e3d9efc
Behzad Azampour <behy1368@gmail.com>
2016-12-02 22:01:02 +0000

    implemented track count and duration calc functionality

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
+function testFunction (ml) {
+  ml.printTracks();
+  ml.printPlaylists();
+  ml.playListArr.forEach((pl) => {
+    pl.printTracks();
+  });
+}
+
 class musicLibrary {
   constructor(libName, creator) {
     this.libName = libName;
     this.creator = creator;
     this.playListArr = [];
+    this.tracksArr = [];
     this.libDuration = 0;
   }
 
-  getLibDuration() {
-    this.libDuration = this.playListArr.reduce((a, b) => {
-      return { plDuration: a.plDuration + b.plDuration };
-    }).plDuration;
+  addTrack(track) {
+    this.tracksArr.push(track);
+    this.calcLibDuration();
+  }
+
+  calcLibDuration() {
+    this.libDuration = this.tracksArr.reduce((trackA, trackB) => {
+      return { length: trackA.length + trackB.length };
+    }).length;
   }
 
   addPlayList(pl) {
     this.playListArr.push(pl);
-    this.getLibDuration();
   }
+
+  printPlaylists() {
+    let output = `Music library ${this.libName} currently contains playlists: \n`;
+    this.playListArr.forEach((pl) => {
+      output += `  - Name: ${pl.plName} \n`;
+    });
+    console.log(output);
+  }
+
+  printTracks() {
+    let output = `Music library ${this.libName} currently contains tracks: \n`;
+    output += `Track Count: ${this.tracksArr.length}, Total Duration: ${this.libDuration} \n`;
+    this.tracksArr.forEach((track) => {
+      output += ` - Title: ${track.title}, Rating: ${track.rating}, Duration: ${track.length} \n`;
+    });
+    console.log(output);
+  }
+
 }
 
 class Playlist {
@@ -28,30 +59,39 @@ class Playlist {
 
   addTrack(track) {
     this.tracksArr.push(track);
-    this.getPlDuration();
-    this.getPlRating();
+    this.calcPlDuration();
+    this.calcPlRating();
   }
 
-  getPlDuration() {
-    this.plDuration = this.tracksArr.reduce((a, b) => {
-      return { trackLength: a.trackLength + b.trackLength };
-    }).trackLength;
+  calcPlDuration() {
+    this.plDuration = this.tracksArr.reduce((trackA, trackB) => {
+      return { length: trackA.length + trackB.length };
+    }).length;
   }
 
-  getPlRating() {
-    let sumRatings = this.tracksArr.reduce((a, b) => {
-      return { trackRating: a.trackRating + b.trackRating };
-    }).trackRating;
+  calcPlRating() {
+    let sumRatings = this.tracksArr.reduce((trackA, trackB) => {
+      return { rating: trackA.rating + trackB.rating };
+    }).rating;
     let countRatings = this.tracksArr.length;
     this.plRating = sumRatings / countRatings;
+  }
+
+  printTracks() {
+    let output = `Playlist ${this.plName} contains tracks: \n`;
+    output += `Track Count: ${this.tracksArr.length}, Total Duration: ${this.plDuration}, Avg Rating: ${this.plRating} \n`;
+    this.tracksArr.forEach((track) => {
+      output += ` - Title: ${track.title}, Rating: ${track.rating}, Duration: ${track.length} \n`;
+    });
+    console.log(output);
   }
 }
 
 class track {
   constructor(title, rating, length) {
-    this.trackTitle = title;
-    this.trackRating = rating;
-    this.trackLength = length;
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
   }
 }
 
@@ -64,6 +104,10 @@ let t1 = new track("a", 4, 30);
 let t2 = new track("b", 4, 60);
 let t3 = new track("c", 5, 50);
 
+ml.addTrack(t1);
+ml.addTrack(t2);
+ml.addTrack(t3);
+
 pl1.addTrack(t1);
 pl1.addTrack(t2);
 pl2.addTrack(t3);
@@ -71,4 +115,4 @@ pl2.addTrack(t3);
 ml.addPlayList(pl1);
 ml.addPlayList(pl2);
 
-console.log(ml.libDuration);
+testFunction(ml);
