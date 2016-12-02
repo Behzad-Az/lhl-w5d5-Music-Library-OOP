84685c972e5161c55d19199bbfe8637136432a4d
Behzad Azampour <behy1368@gmail.com>
2016-12-02 21:12:07 +0000

    working draft on code that meets exercise requirements

---
+++ app.js
@@ -0,0 +1,80 @@
+class musicLibrary {
+  constructor(libName, creator) {
+    this.libName = libName;
+    this.creator = creator;
+    this.playListArr = [];
+    this.libDuration = 0;
+  }
+
+  getLibDuration() {
+    let sumLengths = this.playListArr.reduce((a, b) => {
+      return { plDuration: a.plDuration + b.plDuration };
+    }).plDuration;
+
+    this.libDuration = sumLengths;
+  }
+
+  addPlayList(pl) {
+    this.playListArr.push(pl);
+    this.getLibDuration();
+  }
+}
+
+class Playlist {
+  constructor(name) {
+    this.plName = name;
+    this.tracksArr = [];
+    this.plRating = 0;
+    this.plDuration = 0;
+  }
+
+  addTrack(track) {
+    this.tracksArr.push(track);
+    this.getPlDuration();
+    this.getPlRating();
+  }
+
+  getPlDuration() {
+    let sumLengths = this.tracksArr.reduce((a, b) => {
+      return { trackLength: a.trackLength + b.trackLength };
+    }).trackLength;
+    this.plDuration = sumLengths;
+  }
+
+  getPlRating() {
+    let sumRatings = this.tracksArr.reduce((a, b) => {
+      return { trackRating: a.trackRating + b.trackRating };
+    }).trackRating;
+    let countRatings = this.tracksArr.length;
+    this.plRating = sumRatings / countRatings || `Cannot find rating of playlist ${this.plName}`;
+  }
+}
+
+
+class track {
+  constructor(title, rating, length) {
+    this.trackTitle = title;
+    this.trackRating = rating;
+    this.trackLength = length;
+  }
+}
+
+let ml = new musicLibrary('testMl', 'Behzad');
+
+let pl1 = new Playlist("testPl1");
+let pl2 = new Playlist("testPl2");
+
+let t1 = new track("a", 4, 30);
+let t2 = new track("b", 4, 60);
+let t3 = new track("c", 5, 50);
+
+pl1.addTrack(t1);
+pl1.addTrack(t2);
+pl2.addTrack(t3);
+
+ml.addPlayList(pl1);
+ml.addPlayList(pl2);
+
+console.log(ml.libDuration);
+
+
